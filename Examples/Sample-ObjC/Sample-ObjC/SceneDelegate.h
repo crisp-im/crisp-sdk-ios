2296f27a060012b560fa9c5711108ca57dcc9b43
mb <mb@nesium.com>
2020-03-11 13:55:11 +0000

    added examples

---
+++ Examples/Sample-ObjC/Sample-ObjC/SceneDelegate.h
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.h
+//  Sample-ObjC
+//
+//  Created by Marc Bauer on 19.02.20.
+//  Copyright © 2020 Crisp IM SARL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+@end
---
+++ Examples/Sample-ObjC/Sample-ObjC/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  Sample-ObjC
+//
+//  Created by Marc Bauer on 19.02.20.
+//  Copyright © 2020 Crisp IM SARL. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Crisp/Crisp.h>
+
+@interface ViewController ()
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+}
+
+- (IBAction)presentChat:(id)sender {
+  [self presentViewController:[[CRSPChatViewController alloc] init] animated:YES completion:nil];
+}
+@end
