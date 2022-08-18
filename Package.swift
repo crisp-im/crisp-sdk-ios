a40ff85c62de5fc9847eb75b3c49836703ba38ed
mb <mb@nesium.com>
2022-08-18 16:54:55 +0200

    Updated package manager files for release 1.6.3

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Crisp",
-      url: "https://github.com/crisp-im/crisp-sdk-ios/releases/download/1.6.2/Crisp_1.6.2.zip",
-      checksum: "c3964e30b2c37839a96ff429fca5b17759d78cf0dcdbc61a0ae0c55c908050e1"
+      url: "https://github.com/crisp-im/crisp-sdk-ios/releases/download/1.6.3/Crisp_1.6.3.zip",
+      checksum: "4f8dc1c16f3f36dd265d6d115c88815aacffe6de3c9e0de88934a12ed9f366c7"
     )
   ]
 )
