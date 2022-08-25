03002c77ba841e369542326ff83d7efe6ab59472
mb <mb@nesium.com>
2022-08-25 10:30:15 +0200

    Updated package manager files for release 1.6.4

--- Package.swift
+++ Package.swift
@@ -9,8 +9,12 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Crisp",
-      url: "https://github.com/crisp-im/crisp-sdk-ios/releases/download/1.6.3/Crisp_1.6.3.zip",
-      checksum: "4f8dc1c16f3f36dd265d6d115c88815aacffe6de3c9e0de88934a12ed9f366c7"
+      url: "https://github.com/crisp-im/crisp-sdk-ios/releases/download/1.6.4/Crisp_1.6.4.zip",
+      checksum: "2022-08-25 10:30:14.029 xcodebuild[18874:4152837] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionSentinelHostApplications for extension Xcode.DebuggerFoundation.AppExtensionHosts.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
+2022-08-25 10:30:14.029 xcodebuild[18874:4152837] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionPointIdentifierToBundleIdentifier for extension Xcode.DebuggerFoundation.AppExtensionToBundleIdentifierMap.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
+2022-08-25 10:30:14.590 xcodebuild[18875:4152853] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionSentinelHostApplications for extension Xcode.DebuggerFoundation.AppExtensionHosts.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
+2022-08-25 10:30:14.590 xcodebuild[18875:4152853] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionPointIdentifierToBundleIdentifier for extension Xcode.DebuggerFoundation.AppExtensionToBundleIdentifierMap.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
+389f9afcac376de0c48f2c31964d23d24a5aa4358b2e633c6eeb1abdf2cbdd81"
     )
   ]
 )
