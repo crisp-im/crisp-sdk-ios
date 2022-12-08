ab99cf8e1881be882ec90fa54db4d8aad84a22e6
mb <mb@nesium.com>
2022-12-08 13:32:20 +0700

    Updated package manager files for release 2.0.0-beta.19

--- Package.swift
+++ Package.swift
@@ -1,16 +1,17 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
   name: "Crisp",
+  platforms: [.iOS(.v13)],
   products: [
     .library(name: "Crisp", targets: ["Crisp"])
   ],
   targets: [
     .binaryTarget(
       name: "Crisp",
-      url: "https://github.com/crisp-im/crisp-sdk-ios/releases/download/1.6.4/Crisp_1.6.4.zip",
-      checksum: "389f9afcac376de0c48f2c31964d23d24a5aa4358b2e633c6eeb1abdf2cbdd81"
+      url: "https://github.com/crisp-im/crisp-sdk-ios/releases/download/2.0.0-beta.19/Crisp_2.0.0-beta.19.zip",
+      checksum: "bf7aeab6fa56c8c11dc0be58a4d16d97fda1e51c2d3721635afb78d0277819b0"
     )
   ]
 )
