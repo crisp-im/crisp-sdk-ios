e5e5b738a15139689bdd9257883777d5d229b8b3
mb <mb@nesium.com>
2022-12-16 16:47:12 +0700

    Updated package manager files for release 2.0.0-beta.24

--- Crisp.podspec
+++ Crisp.podspec
@@ -1,13 +1,30 @@
 Pod::Spec.new do |spec|
   spec.name                = "Crisp"
-  spec.version             = "2.0.0-beta.20"
+  spec.version             = "2.0.0-beta.24"
   spec.summary             = "Crisp SDK for iOS."
   spec.description         = "Crisp SDK for iOS, used for visitors to get help from operators."
   spec.homepage            = "https://crisp.chat"
   spec.author              = "Crisp IM SAS"
   spec.platform            = :ios, "13.0"
   spec.license             = { :type => "Commercial" }
-  spec.source              = { :http => "https://github.com/crisp-im/crisp-sdk-ios/releases/download/2.0.0-beta.20/Crisp_2.0.0-beta.20.zip" }
-  spec.vendored_frameworks = "Crisp.xcframework"
-  spec.preserve_paths      = "Crisp.xcframework"
+  spec.source              = { :http => "https://github.com/crisp-im/crisp-sdk-ios/releases/download/2.0.0-beta.24/Crisp_cocoapods_2.0.0-beta.24.zip" }
+  
+  spec.default_subspecs    = "Crisp"
+  
+  spec.subspec "Crisp" do |s|
+    s.vendored_frameworks  = "Crisp.xcframework"
+    s.preserve_paths       = "Crisp.xcframework"
+  end
+  
+  spec.subspec "CrispWebRTC" do |s|
+    s.vendored_frameworks  = "CrispWebRTC.xcframework"
+    s.preserve_paths       = "CrispWebRTC.xcframework"
+    
+    s.dependency "Crisp/WebRTC"
+  end
+  
+  spec.subspec "WebRTC" do |s|
+    s.vendored_frameworks  = "WebRTC.xcframework"
+    s.preserve_paths       = "WebRTC.xcframework"
+  end
 end
--- Package.swift
+++ Package.swift
@@ -5,13 +5,37 @@ let package = Package(
   name: "Crisp",
   platforms: [.iOS(.v13)],
   products: [
-    .library(name: "Crisp", targets: ["Crisp"])
+    .library(name: "Crisp", targets: ["CrispTarget"]),
+    .library(name: "CrispWebRTC", targets: ["CrispWebRTCTarget"])
   ],
   targets: [
+    .target(
+      name: "CrispTarget",
+      dependencies: [
+        .target(name: "Crisp")
+      ]
+    ),
+    .target(
+      name: "CrispWebRTCTarget",
+      dependencies: [
+        .target(name: "CrispWebRTC"),
+        .target(name: "WebRTC")
+      ]
+    ),
     .binaryTarget(
       name: "Crisp",
-      url: "https://github.com/crisp-im/crisp-sdk-ios/releases/download/2.0.0-beta.20/Crisp_2.0.0-beta.20.zip",
-      checksum: "abbe9e4884bc9e2a62455d226a59755a3bdcb8f688b085a428496cf6202df5c5"
-    )
+      url: "https://github.com/crisp-im/crisp-sdk-ios/releases/download/2.0.0-beta.24/Crisp_2.0.0-beta.24.zip",
+      checksum: "f53b060799416bf7146eb34240853ec6b4963e3da95f0783a9daf056b70760f2"
+    ),
+    .binaryTarget(
+      name: "CrispWebRTC",
+      url: "https://github.com/crisp-im/crisp-sdk-ios/releases/download/2.0.0-beta.24/CrispWebRTC_2.0.0-beta.24.zip",
+      checksum: "20830485137cb02242c153b641a1376a71a1b663fa696c3aade3bc83f7860bac"
+    ),
+    .binaryTarget(
+      name: "WebRTC",
+      url: "https://github.com/crisp-im/crisp-sdk-ios/releases/download/2.0.0-beta.24/WebRTC_2.0.0-beta.24.zip",
+      checksum: "8d98e51d52f88a480db64724e3a39f870fc8f7235a9bebc91697e35b4a8abc55"
+    ),
   ]
 )
